5b2dc2de5cf77db5a0fff5b73ceba77469f3747d
Maria Nesterova <82315940+ridebyhorse@users.noreply.github.com>
2024-08-19 21:07:09 +0300

    create no-storyboard base, add .gitignore

---
+++ MobileUpGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileUpGallery
+//
+//  Created by Мария Нестерова on 19.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
