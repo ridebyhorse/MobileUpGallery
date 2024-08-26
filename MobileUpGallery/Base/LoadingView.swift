ff3e08b195121c163777249610456f8201ead94a
Maria Nesterova <82315940+ridebyhorse@users.noreply.github.com>
2024-08-26 03:38:48 +0300

    start error presenting

--- MobileUpGallery/Base/LoadingView.swift
+++ MobileUpGallery/Base/LoadingView.swift
@@ -51,14 +51,3 @@ class LoadingView: UIView {
     }
     
 }
-
-extension UIImageView {
-    func rotate() {
-        let rotation : CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
-        rotation.toValue = NSNumber(value: Double.pi * 2)
-        rotation.duration = 2
-        rotation.isCumulative = true
-        rotation.repeatCount = Float.greatestFiniteMagnitude
-        self.layer.add(rotation, forKey: "rotationAnimation")
-    }
-}
